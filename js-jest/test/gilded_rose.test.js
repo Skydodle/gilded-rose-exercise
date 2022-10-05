b3ef89cf9fe44496f40ab56706615ea6a9fd3cc9
skydodle <skydodle@gmail.com>
2022-10-04 17:57:37 -0700

    Created basic test for each item type with name, sellIn, & qlt to check if func works

--- js-jest/test/gilded_rose.test.js
+++ js-jest/test/gilded_rose.test.js
@@ -1,9 +1,47 @@
 const { Shop, Item } = require("../src/gilded_rose");
 
 describe("Gilded Rose", function () {
-  it("should foo", function () {
-    const gildedRose = new Shop([new Item("foo", 0, 0)]);
-    const items = gildedRose.updateQuality();
-    expect(items[0].name).toBe("foo");
+  describe("Regular Items", function () {
+    it("should return correct name, sellIn, quality", function () {
+      const gildedRose = new Shop([new Item("Elixir of the Mongoose", 5, 7)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("Elixir of the Mongoose");
+      expect(items[0].sellIn).toBe(4);
+      expect(items[0].quality).toBe(6);
+    });
+  });
+
+  describe("Aged Brie", function () {
+    it("should return correct name, sellIn, quality", function () {
+      const gildedRose = new Shop([new Item("Aged Brie", 2, 0)]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("Aged Brie");
+      expect(items[0].sellIn).toBe(1);
+      expect(items[0].quality).toBe(1);
+    });
+  });
+
+  describe("Sulfuras, Hand of Ragnaros", function () {
+    it("should return correct name, sellIn, quality", function () {
+      const gildedRose = new Shop([
+        new Item("Sulfuras, Hand of Ragnaros", 0, 80),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("Sulfuras, Hand of Ragnaros");
+      expect(items[0].sellIn).toBe(0);
+      expect(items[0].quality).toBe(80);
+    });
+  });
+
+  describe("Backstage passes to a TAFKAL80ETC concert", function () {
+    it("should return correct name, sellIn, quality", function () {
+      const gildedRose = new Shop([
+        new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+      ]);
+      const items = gildedRose.updateQuality();
+      expect(items[0].name).toBe("Backstage passes to a TAFKAL80ETC concert");
+      expect(items[0].sellIn).toBe(14);
+      expect(items[0].quality).toBe(21);
+    });
   });
 });
