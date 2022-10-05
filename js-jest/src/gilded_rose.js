737df251c43100990bb6e27421d6d4dbe9d5a580
skydodle <skydodle@gmail.com>
2022-10-04 18:42:36 -0700

    Changed for loop to forEach so I can use the item variable

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -11,85 +11,80 @@ class Shop {
     this.items = items;
   }
   updateQuality() {
-    for (let i = 0; i < this.items.length; i++) {
-      if (this.items[i].name == "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn--;
+    this.items.forEach((item) => {
+      if (item.name == "Sulfuras, Hand of Ragnaros") {
+        item.sellIn--;
       }
       // REGULAR ITEMS quality -1 per day
       // if item is not brie and not concert pass (these two increase qlt per day instead)
       if (
-        this.items[i].name != "Aged Brie" &&
-        this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
+        item.name != "Aged Brie" &&
+        item.name != "Backstage passes to a TAFKAL80ETC concert"
       ) {
         // quality is greater than 0
-        if (this.items[i].quality > 0) {
+        if (item.quality > 0) {
           // not sulfuras
-          if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
+          if (item.name != "Sulfuras, Hand of Ragnaros") {
             // decrement quality by 1 per day
-            this.items[i].quality = this.items[i].quality - 1;
+            item.quality = item.quality - 1;
           }
         }
         // NOT REGULAR ITEMS (brie & pass)
       } else {
         // quality is never over 50
-        if (this.items[i].quality < 50) {
+        if (item.quality < 50) {
           // increase quality by 1 per day
-          this.items[i].quality = this.items[i].quality + 1;
+          item.quality = item.quality + 1;
           // if its a pass
-          if (
-            this.items[i].name == "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (item.name == "Backstage passes to a TAFKAL80ETC concert") {
             // 10 days or less
-            if (this.items[i].sellIn < 11) {
+            if (item.sellIn < 11) {
               // increase another 1 if under 50 (+2)
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
             // 5 day or less
-            if (this.items[i].sellIn < 6) {
+            if (item.sellIn < 6) {
               // increase another 1 if under 50 (+3)
-              if (this.items[i].quality < 50) {
-                this.items[i].quality = this.items[i].quality + 1;
+              if (item.quality < 50) {
+                item.quality = item.quality + 1;
               }
             }
           }
         }
       }
       // decrement sellIn if not Sulfuras
-      if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-        this.items[i].sellIn = this.items[i].sellIn - 1;
+      if (item.name != "Sulfuras, Hand of Ragnaros") {
+        item.sellIn = item.sellIn - 1;
       }
       // if expired
-      if (this.items[i].sellIn < 0) {
+      if (item.sellIn < 0) {
         // not brie
-        if (this.items[i].name != "Aged Brie") {
+        if (item.name != "Aged Brie") {
           // not pass
-          if (
-            this.items[i].name != "Backstage passes to a TAFKAL80ETC concert"
-          ) {
+          if (item.name != "Backstage passes to a TAFKAL80ETC concert") {
             // quality more than 0 and not sulfuras
             // quality decrease 1 more per day (double)
-            if (this.items[i].quality > 0) {
-              if (this.items[i].name != "Sulfuras, Hand of Ragnaros") {
-                this.items[i].quality = this.items[i].quality - 1;
+            if (item.quality > 0) {
+              if (item.name != "Sulfuras, Hand of Ragnaros") {
+                item.quality = item.quality - 1;
               }
             }
             // yes pass
           } else {
             // set quality to zero
-            this.items[i].quality =
-              this.items[i].quality - this.items[i].quality;
+            item.quality = item.quality - item.quality;
           }
           // yes brie
           // } else {
-          //   if (this.items[i].quality < 50) {
+          //   if (item.quality < 50) {
           //     // increase quality by 1 more ???
-          //     this.items[i].quality = this.items[i].quality + 1;
+          //     item.quality = item.quality + 1;
           //   }
         }
       }
-    }
+    });
 
     return this.items;
   }
